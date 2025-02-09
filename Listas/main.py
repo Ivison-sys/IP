518660b816943cc7d47d75f135422a0b22f83813
Ivison-sys <irs3@cin.ufpe.br>
2025-02-09 21:20:29 +0000

    A questão 10 está com um erropython3 ./Listas/main.py < ./Listas/inp.txt > ./Listas/out.txt

--- Listas/main.py
+++ Listas/main.py
@@ -1,95 +1,111 @@
-print("QUE COMECEM AS BATALHAS")
-inp = input().split(" ")
 
-qAsh , qGary = int(inp[0]), int(inp[1])
-pokemonsAsh = []
-nomesAsh = []
-pokemonsGary = []
-nomesGary = []
-vitoriasAsh = 0
-vitoriasGary = 0
-batalhas = [] 
-if qAsh == qGary and qGary == 0:
-  print("=============== ===============")
-  print("Nenhuma batalha foi concluída.")
+nomeFeiticeiro = input()
+vidaFeiticeiro = int(input())
+ataqueFeiticeiro = int(input())
+defesaFeiticeiro = int(input())
+reversaoFeitico = input() == "True"
+expansaoDominio = input() == "True"
+
+vidaMahoraga = int(input())
+ataqueMahoraga = int(input())
+defesaMahoraga = int(input())
+
+listaGolpes = input().split(", ")
+
+if reversaoFeitico:
+    print("Mesmo com a regeneração, ainda não vai ser fácil! Vamos nessa!")
 else:
-  if qGary == 0:
-    print("=============== ===============")
-    print("Gary deixou seus pokemons descansando!")
-    print("Ash é o grande vencedor!")
-  elif qAsh == 0:
-    print("=============== ===============")
-    print("Ash deixou seus pokemons descansando!")
-    print("Gary é o grande vencedor!")
-  else:
-    for x in range(int(qAsh)):
-      pokemon = input().split(", ")
-      pokemon[2], pokemon[3] = int(pokemon[2]), int(pokemon[3])
-      pokemonsAsh.append(pokemon)   
-      nomesAsh.append(pokemon[0])
-    for x in range(int(qGary)):
-      pokemon = input().split(", ")
-      pokemon[2], pokemon[3] = int(pokemon[2]), int(pokemon[3])
-      pokemonsGary.append(pokemon)
-      nomesGary.append(pokemon[0])
+    print("Exorcizar o Mahoraga sem conseguir me curar vai ser bem difícil, mas eu não tenho escolha!")
 
-    decisao = input()
-    while decisao != "FIM DAS BATALHAS":
-    
-      inp = input().split(" ")
-      sum = int(inp[0]) + int(inp[1])
+adaptacoes_nome = []
+adaptacoes_vezes = []
 
-      isFishtAsh = False
 
-      if (sum % 2 == 0 and decisao == "par") or (sum % 2 == 1 and decisao == "ímpar"):
-        isFishtAsh = True
-    
-      inp = input().split(" ")
-      ativoAsh = inp[0]
-      inp = input().split(" ")
-      ativoGary = inp[0]
+while vidaFeiticeiro > 0 and vidaMahoraga > 0:
+    movimentoFeiticeiro = input()
 
-      indiceAsh = nomesAsh.index(ativoAsh)
-      indiceGary = nomesGary.index(ativoGary)
-    
-      contador = 0
-      while(pokemonsAsh[indiceAsh][2] > 0 and pokemonsGary[indiceGary][2] > 0):
-        if isFishtAsh:
-          if contador % 2 == 0:
-            pokemonsGary[indiceGary][2] -= pokemonsAsh[indiceAsh][3] * 2
-          else:
-            pokemonsAsh[indiceAsh][2] -= pokemonsGary[indiceGary][3] * 2
+    if movimentoFeiticeiro == "expansão de domínio":
+        if expansaoDominio:
+            if nomeFeiticeiro == "Satoru Gojo":
+                print(f"Como assim o Mahoraga já se adaptou ao infinito de {nomeFeiticeiro}!?")
+            else:
+                print("Nem mesmo a sua adaptação pode derrotar isto!")
+                print(f"{nomeFeiticeiro} conseguiu!")
+                if nomeFeiticeiro == "Megumi Fushiguro":
+                    print("Depois de muito tempo, finalmente o Mahoraga foi exorcizado. Fushiguro é o primeiro usuário das dez sombras a conseguir esse feito!")
+                elif nomeFeiticeiro == "Sukuna":
+                    print("Você me mostrou o caminho, Megumi Fushiguro, e por isso eu sou grato!")
+                else:
+                    print("Depois de muito tempo, finalmente o Mahoraga foi exorcizado, mas Fushiguro não participou da luta, logo o ritual foi anulado.")
+                break
         else:
-          if contador % 2 == 1:
-            pokemonsGary[indiceGary][2] -= pokemonsAsh[indiceAsh][3] * 2
-          else:
-            pokemonsAsh[indiceAsh][2] -= pokemonsGary[indiceGary][3] * 2
-      
-        contador+=1
-      decisao = input()
+            print("Droga. Eu não aprendi a expandir meu domínio ainda!")
+            continue
 
-      if pokemonsAsh[indiceAsh][2] < pokemonsGary[indiceGary][2]:    
-        print(f"{pokemonsAsh[indiceAsh][0]} desmaiou e {pokemonsGary[indiceGary][0]} venceu esta luta") 
-        p1, p2 = pokemonsAsh[indiceAsh][0].lower(), pokemonsGary[indiceGary][0].upper()
-        batalhas.append([p1, p2])
-        vitoriasGary+=1
-        del pokemonsAsh[indiceAsh]
-        del nomesAsh[indiceAsh]
-      else:
-        print(f"{pokemonsGary[indiceGary][0]} desmaiou e {pokemonsAsh[indiceAsh][0]} venceu esta luta")
-        p1, p2 = pokemonsAsh[indiceAsh][0].upper(), pokemonsGary[indiceGary][0].lower()
-        batalhas.append([p1, p2])
-        vitoriasAsh+=1
-        del pokemonsGary[indiceGary]
-        del nomesGary[indiceGary]
-
-    print("=============== ===============")
-    for i in range(len(batalhas)):
-      print(f"{i+1}° Batalha: {batalhas[i][0]} vs {batalhas[i][1]}")
-
-    if vitoriasAsh > vitoriasGary:
-      print("Ash é o grande vencedor!")
-    elif vitoriasGary > vitoriasAsh:
-      print("Gary é o grande vencedor!")
+    if movimentoFeiticeiro == "black flash":
+        print("As faíscas negras ignoram qualquer tipo de defesa! Toma essa Mahoraga!")
+        dano = (ataqueFeiticeiro + 25) * 2
+    elif movimentoFeiticeiro == "reversão de feitiço":
+        if reversaoFeitico:
+            vidaFeiticeiro += 25
+            print("Eu posso continuar lutando mais um pouco...")
+        continue
+    elif movimentoFeiticeiro not in listaGolpes:
+        print("Eu não sei que ideia é essa de tentar usar um golpe que eu não domino!")
+        continue
     else:
-      print("Depois de todas as batalhas, ainda terminou em empate!")
+        fator = 1
+        if movimentoFeiticeiro in adaptacoes_nome:
+            indice = adaptacoes_nome.index(movimentoFeiticeiro)
+            adaptacoes_vezes[indice] += 1
+            if adaptacoes_vezes[indice] == 2:
+                print(f"A roda do Mahoraga girou pela segunda vez! {movimentoFeiticeiro} só vai funcionar mais uma vez")
+                fator = 2
+            elif adaptacoes_vezes[indice] == 3:
+                print(f"A roda do Mahoraga girou pela terceira vez! {movimentoFeiticeiro} não vai funcionar mais")
+                fator = 4
+            else:
+                print("Esse ataque é inútil! Melhor tentar outra coisa.")
+                continue
+        else:
+            print(f"A roda do Mahoraga girou uma vez! {movimentoFeiticeiro} só vai funcionar mais duas vezes")
+            adaptacoes_nome.append(movimentoFeiticeiro)
+            adaptacoes_vezes.append(1)
+        dano = max(((ataqueFeiticeiro - defesaMahoraga) + 25) // fator, 0)
+    
+    vidaMahoraga -= dano
+    if vidaMahoraga <= 0:
+        print(f"{nomeFeiticeiro} conseguiu!")
+        if nomeFeiticeiro == "Megumi Fushiguro":
+            print("Depois de muito tempo, finalmente o Mahoraga foi exorcizado. Fushiguro é o primeiro usuário das dez sombras a conseguir esse feito!")
+        elif nomeFeiticeiro == "Sukuna":
+            print("Você me mostrou o caminho, Megumi Fushiguro, e por isso eu sou grato!")
+        elif nomeFeiticeiro == "Satoru Gojo":
+            print("Nem você sua adaptação é páreo para o infinito, queridinho.")
+        else:
+            print("Depois de muito tempo, finalmente o Mahoraga foi exorcizado, mas Fushiguro não participou da luta, logo o ritual foi anulado.")
+        break
+
+    movimentoMahoraga = input()
+    if movimentoMahoraga == "ataque":
+        vidaFeiticeiro -= max(((ataqueMahoraga - defesaFeiticeiro) + 25), 0)
+    elif movimentoMahoraga == "regeneração":
+        vidaMahoraga += 25
+        print("Ele está se regenerando.")
+    elif movimentoMahoraga == "adaptação":
+        if movimentoFeiticeiro == "black flash":
+            print("Nem você vai conseguir se adaptar a isso, mahoraga!")
+        elif movimentoFeiticeiro in adaptacoes_nome:
+            indice = adaptacoes_nome.index(movimentoFeiticeiro)
+            adaptacoes_vezes[indice] += 1
+            if adaptacoes_vezes[indice] == 2:
+                print(f"A roda do Mahoraga girou pela segunda vez! {movimentoFeiticeiro} só vai funcionar mais uma vez")
+            elif adaptacoes_vezes[indice] == 3:
+                print(f"A roda do Mahoraga girou pela terceira vez! {movimentoFeiticeiro} não vai funcionar mais")
+
+    if vidaFeiticeiro <= 0:
+        if nomeFeiticeiro == "Satoru Gojo":
+            print("Magnífico, Satoru Gojo. Lembrarei de você enquanto eu durar nesta vida.")
+        else:
+            print(f"Parece que nem mesmo {nomeFeiticeiro} foi páreo contra o Mahoraga...")
+        break
