237b758284b01c59714b12fb47e85cd5d49aad5f
Ivison-sys <irs3@cin.ufpe.br>
2025-02-13 15:23:31 +0000

    Feitas: 4

--- Funcoes/main.py
+++ Funcoes/main.py
@@ -1,6 +1,28 @@
 def descriptografar(palavra, num):
-  pass
+  texto = ""
+  for i in range(len(palavra)):
+    indice = alfabeto.index(palavra[i])
 
+    if indice - num < 0:
+      letra = 26 + (indice - num)
+    else:
+      letra = indice - num
+    
+    texto += alfabeto[letra]
+  return texto
+
+def calcular_dano(precisao, poder_explosao, resistencia, palavra):
+  if palavra == "ALTO":
+    fator = 2
+  elif palavra == "MEDIO":
+    fator = 1.5
+  else:
+    fator = 1
+  
+  return round(precisao * (poder_explosao / resistencia) * fator)
+
+def ordenarAtaques(lista_ataques):
+  return sorted(ataques, key= lambda x: x[1], reverse=True)
 n_ataques = int(input())
 alfabeto = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
@@ -11,5 +33,22 @@ else:
   ataques = []
   for i in range(n_ataques):
     ataque = input().split(', ')
-    ataques.append(ataques)
-   
+    palavra = descriptografar(ataque[4], int(ataque[5]))
+    dano = calcular_dano(int(ataque[1]), int(ataque[3]),int(ataque[2]), palavra)
+    ataques.append([ataque[0], dano])
+    
+    ataques = ordenarAtaques(ataques)
+
+    print(f"Decifrando: {palavra}")
+    print(f"{ataque[0]}: {dano} de dano calculado.")
+
+    if dano >= 100:
+      print(f"{ataque[0]} será destruído!")
+    else:
+      print(f"{ataque[0]} resistirá ao ataque.")
+    print()
+    
+  print("Prioridade de ataques:")
+
+  for at in ataques:
+    print(f"{at[0]} - {at[1]} de dano")
